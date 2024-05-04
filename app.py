128fd17832416c8fd93f7622eff59da69959e635
Aarthi1209 <89206591+Aarthi1209@users.noreply.github.com>
2024-05-04 11:28:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def fetch_poster(movie_id):
      full_path = "https://image.tmdb.org/t/p/w500/"+poster_path
      return full_path
 
-movies = pickle.load(open("movies_list.pkl", 'rb'))
+movies = pickle.load(bz2.BZ2File("movies_list.pkl", 'rb'))
 similarity = pickle.load(bz2.BZ2File("similarity.pkl", 'rb'))
 movies_list=movies['title'].values
 
